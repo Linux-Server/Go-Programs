38ae8f0805cb33b2322ddc690fc811a912fa22b2
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-05-03 11:02:42 +0530

    scan the input from command line

--- go-essentials/investment_calculator.go
+++ go-essentials/investment_calculator.go
@@ -7,10 +7,27 @@ import (
 
 func main() {
 	fmt.Println("hello go essentials!")
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	var investmentAmount float64
+	var expectedReturnRate float64 // Short assignment operator
+	var years float64
+	fmt.Println("Enter the investment amount ?")
+	fmt.Scan(&investmentAmount)
 
-	var futureValue = investmentAmount * (math.Pow(1+expectedReturnRate/100, years))
+	fmt.Println("enter the return rate ? ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Println("enter the years ? ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * (math.Pow(1+expectedReturnRate/100, years))
 	fmt.Println("The value is : ", futureValue)
+	// variableDeclararion()
+}
+
+func variableDeclararion() {
+	name, age := "sam", 12
+	var catagory, status string = "sam", "ray"
+	fmt.Printf("The name is %v and age is %v\n", name, age)
+	fmt.Printf("The name is %v and age is %v\n", catagory, status)
+
 }
